7e13e7cb22fa0081bd0fa8853ad2855211a56cfa
Jeffrey <jeffreyho2002@gmail.com>
2024-03-31 18:24:27 +0800

    Broadcasting messages to all users through useRef useEffect

--- client/src/routes/voice-chat.jsx
+++ client/src/routes/voice-chat.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 const VoiceChat = () => {
     // Code for WebSocket connection (note 'ws' only works in the server, need to use native WebSocket object for frontend)
+    /*
+    Legacy code (in case it is needed later)
     const ws = new WebSocket("ws://localhost:8000")
     ws.addEventListener('open', () => {
         console.log("Connected to server!")
     })
+    */
+    const ws = useRef(null)
+    useEffect(() => {
+        ws.current = new WebSocket("ws://localhost:8000")
+        ws.current.onopen = () => {
+            console.log("Connected to server!")
+        }
+
+        // If a message is received, append it to the chat room
+        ws.current.onmessage = ({ data }) => {
+            console.log(data)
+            const li = document.createElement("li")
+            li.textContent = data
+            document.querySelector(".sent-messages").appendChild(li)
+        }
+
+        // Handle closing of WebSocket
+        return () => {
+            ws.current.close()
+        }
+    }, [])
 
     // Code for form element
     const [text, setText] = useState("")
@@ -14,26 +37,21 @@ const VoiceChat = () => {
         e.preventDefault()
         
         // Send message to the server here...
+        // Only sends the message if it is not empty
         if (text) {
-            ws.send(text)
+            ws.current.send(text)
         }
 
         setText("")
     }
 
-    const receiveMessage = () => {
-
-    }
-
     return (
         <>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" value={text} placeholder="Enter text" onChange={e => setText(e.target.value)}/>
-                    <button type="submit">Submit</button>
-                </form>
-                <ul className="sent-messages"></ul>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={text} placeholder="Enter text" onChange={e => setText(e.target.value)}/>
+                <button type="submit">Submit</button>
+            </form>
+            <ul className="sent-messages"></ul>
         </>
     )
 }
--- server/index.js
+++ server/index.js
@@ -2,13 +2,19 @@ const WebSocket = require ('ws')
 
 const wss = new WebSocket.Server({ port: 8000 })
 
+
 wss.on('connection', ws => {
 
     console.log("a user has connected :))")
 
     // Listen for messages being sent to server from clients
-    ws.on('message', data => {
-        console.log("Message received: " + data)
+    ws.on('message', message => {
+        console.log(`Broadcasting ${message} to all users`) 
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message.toString())
+            }
+        })
     })
 
     // Listen for close
