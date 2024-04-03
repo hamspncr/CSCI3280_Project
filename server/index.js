679e6045a8e5f45728a4367d136ac56fd4e0e360
1155163071@link.cuhk.edu.hk <1155163071@link.cuhk.edu.hk>
2024-04-03 20:30:48 +0800

    Basic room functionality

--- server/index.js
+++ server/index.js
@@ -2,23 +2,72 @@ const WebSocket = require ('ws')
 
 const wss = new WebSocket.Server({ port: 8000 })
 
+const rooms = {
+    "0b01c8f2-e484-41a4-932a-e1af9f959d0b": {
+        name: "test-room",
+        users: [],
+        messages: []
+    } 
+}
+
+const broadcastRoomInfo = (rooms) => {
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(rooms))
+        }
+    })
+}
 
 wss.on('connection', ws => {
 
-    console.log("a user has connected :))")
+    console.log("User connected")
 
-    // Listen for messages being sent to server from clients
     ws.on('message', message => {
-        console.log(`Broadcasting ${message} to all users`) 
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message.toString())
+        const {event, payload} = JSON.parse(message)
+        if (event === 'create-room') {
+            const {name} = payload
+            const id = crypto.randomUUID();
+            rooms[id] = {
+                name: name,
+                users: [],
+                messages: []
             }
-        })
+
+            broadcastRoomInfo(rooms)
+
+            console.log(`Room created with id ${id}, named ${name}`)
+        } else if (event === 'join-room') {
+            const {id} = payload
+            const room = rooms[id]
+            
+            if (room) {
+                room.users.push(ws);
+                console.log(`User joined ${room.name}`)
+            } else {
+                console.log(`Room not found`)
+            }
+
+            broadcastRoomInfo(rooms)
+
+        } else if (event === 'send-message') {
+            const {username, id, text} = payload
+            const room = rooms[id]
+
+            if (room) {
+                const msg = {
+                    name: username,
+                    content: text
+                }
+                room.messages.push(msg)
+            } else {
+                console.log(`Room not found`)
+            }
+
+            broadcastRoomInfo(rooms)
+        }
     })
 
-    // Listen for close
     ws.on('close', () => {
-        console.log("byebye user :(")
+        console.log("User disconnected")
     })
 })
