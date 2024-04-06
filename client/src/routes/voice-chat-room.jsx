83564fcd7010c12b7f67d3f3b95b3b3798bea25b
1155163071@link.cuhk.edu.hk <1155163071@link.cuhk.edu.hk>
2024-04-06 22:41:38 +0800

    Working webrtc(peerjs) audio streaming per room

--- client/src/routes/voice-chat-room.jsx
+++ client/src/routes/voice-chat-room.jsx
@@ -1,3 +1,4 @@
+import Peer from "peerjs";
 import { useContext, useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { UsernameContext } from "../main";
@@ -11,15 +12,23 @@ const VoiceChatRoom = () => {
   const [message, setMessage] = useState("");
   const navigate = useNavigate();
   const ws = useRef(null);
+  const peer = useRef(null);
+  const myPeerId = useRef(null);
+  const myStream = useRef(null);
+  const activeCalls = useRef({});
 
   useEffect(() => {
     ws.current = new WebSocket("ws://localhost:8000");
-    ws.current.onopen = () => {
+    ws.current.onopen = async () => {
       setLoading(false);
       if (!username) {
         console.log("Something bad must've happened");
         navigate("/voice-chat");
       } else {
+        myStream.current = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        });
+
         const get_room = {
           event: "get-room",
           payload: {
@@ -29,28 +38,65 @@ const VoiceChatRoom = () => {
 
         ws.current.send(JSON.stringify(get_room));
 
-        const join_room = {
-          event: "join-room",
-          payload: {
-            id: roomID,
-            username: username,
-          },
-        };
+        peer.current = new Peer(undefined, {
+          host: "localhost",
+          port: 8001,
+          path: "/peer-server",
+        });
 
-        console.log(join_room);
-        ws.current.send(JSON.stringify(join_room));
+        peer.current.on("open", (id) => {
+          myPeerId.current = id;
+          const join_room = {
+            event: "join-room",
+            payload: {
+              id: roomID,
+              username: username,
+              peerId: id,
+            },
+          };
+          ws.current.send(JSON.stringify(join_room));
+
+          peer.current.on("call", (call) => {
+            call.answer(myStream.current);
+            call.on("stream", (remoteStream) => {
+              const incomingAudio = new Audio();
+              incomingAudio.volume = 0.02;
+              incomingAudio.srcObject = remoteStream;
+              incomingAudio.play();
+            });
+          });
+        });
       }
     };
 
     ws.current.onmessage = ({ data }) => {
       const { event, payload } = JSON.parse(data);
       if (event === "join-room") {
-        setJoined(true);
         setRoomInfo(payload);
+        if (!joined) {
+          setJoined(true);
+          payload.users.forEach((user) => {
+            if (user.peerId !== myPeerId.current) {
+              const call = peer.current.call(user.peerId, myStream.current);
+              activeCalls.current[user.peerId] = call;
+              call.on("stream", (remoteStream) => {
+                const incomingAudio = new Audio();
+                incomingAudio.volume = 0.02;
+                incomingAudio.srcObject = remoteStream;
+                incomingAudio.play();
+              });
+            }
+          });
+        }
       } else if (event === "get-room") {
         setRoomInfo(payload);
       } else if (event === "leave-room") {
-        setRoomInfo(payload);
+        const { newRoom, leaver } = payload;
+        console.log(leaver)
+        setRoomInfo(newRoom);
+
+        activeCalls.current[leaver.peerId].close();
+        delete activeCalls.current[leaver.peerId];
       } else if (event === "send-message") {
         setRoomInfo((prev) => ({
           ...prev,
@@ -60,8 +106,18 @@ const VoiceChatRoom = () => {
     };
 
     return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      Object.values(activeCalls.current).forEach((call) => call.close());
+
+      if (peer.current) {
+        peer.current.destroy();
+      }
+      if (myStream.current) {
+        myStream.current = null;
+      }
       ws.current.close();
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleMessage = (e) => {
@@ -95,7 +151,7 @@ const VoiceChatRoom = () => {
     };
 
     ws.current.send(JSON.stringify(leave_room));
-    navigate("/voice-chat")
+    navigate("/voice-chat");
   };
 
   return (
